5d194587998a4dc519b054e73005dc31bd241504
Daniel Troger <daniel@natur-kultur.eu>
2014-04-21 13:00:50 +0200

    first commit

---
+++ main.php
@@ -0,0 +1,43 @@
+<?php
+$wtd = array("generating","creating","hacking","chmodding","deleting");
+for($i = 0;$i <= 100;$i++)
+{
+echo $wtd[rand(0,(sizeof($wtd)-1))] . " " . fillout(rs(rand(1,20)) . "." . rs(3) . "....",100) . "\n";
+for($is = 0; $is <= $i;$is++)
+{
+echo "=";
+}
+echo ">";
+for($t=$i;$t <100;$t++)
+{
+echo " ";
+}
+echo "{$i}%\r";
+usleep(rand(10000,500000));
+}
+echo "\n";
+function rs($length = 10) {
+    $characters = 'abcdefghijklmnopqrstuvwqyz';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+function fillout($string,$totleng)
+{
+$leng = strlen($string);
+if($leng >=$totleng)
+{
+return $string;
+}
+else
+{
+$res = $string;
+for($a = $leng; $a < $totleng; $a++)
+{
+$res .= " ";
+}
+return $res;
+}
+}
